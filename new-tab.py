07623dbe05af31bb887913f4bfe5f47f31d8a7a4
dvlrr <80828601+dvlrr@users.noreply.github.com>
2023-01-13 08:59:00 +0000

    Version 1.0

---
+++ new-tab.py
@@ -0,0 +1,16 @@
+import webbrowser
+chrome = webbrowser.get('chrome') # or webbrowser.get('chrome')
+chrome.open_new_tab('chrome://newtab')
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ secrets.py
@@ -0,0 +1,5 @@
+sender_address = "YOUR EMAIL ADDRESS"
+sender_password = "YOUR PASSWORD/GMAIL API KEY"
+sender_server = 'smtp.gmail.com'
+sender_port = 587
+recipient_address = "RECIPIENT EMAIL ADDRESS"
